3347c55f47d750cfae511b8ff8e554edbd719430
Harsh Singh <70288936+harxhsingh@users.noreply.github.com>
2021-09-27 08:21:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+console.log('This is my project 6 from JavaScript course');
+
 // Utility functions:
 // 1. Utility function to get DOM element from string
 function getElementFromString(string) {
@@ -54,3 +56,67 @@ addParam.addEventListener('click', () => {
     }
     addedParamCount++;
 })
+
+// If the user clicks on submit button
+let submit = document.getElementById('submit');
+submit.addEventListener('click', () => {
+    // Show please wait in the response box to request patience from the user
+    // document.getElementById('responseJsonText').value = "Please wait.. Fetching response...";
+    document.getElementById('responsePrism').innerHTML = "Please wait.. Fetching response...";
+
+    // Fetch all the values user has entered
+    let url = document.getElementById("url").value;
+    let requestType = document.querySelector("input[name='requestType']:checked").value;
+    let contentType = document.querySelector("input[name='contentType']:checked").value;
+
+    // If user has used params option instead of json, collect all the parameters in an object
+    if (contentType == 'params') {
+        data = {};
+        for (let i = 0; i < addedParamCount + 1; i++) {
+            if (document.getElementById('parameterKey' + (i + 1)) != undefined) {
+                let key = document.getElementById('parameterKey' + (i + 1)).value;
+                let value = document.getElementById('parameterValue' + (i + 1)).value;
+                data[key] = value;
+            }
+        }
+        data = JSON.stringify(data);
+    } else {
+        data = document.getElementById('requestJsonText').value;
+    }
+
+    // Log all the values in the console for debugging
+    console.log('URL is ', url);
+    console.log('requestType is ', requestType);
+    console.log('contentType is ', contentType);
+    console.log('data is ', data);
+
+    // if the request type is get, invoke fetch api to create a post request
+    if (requestType == 'GET') {
+        fetch(url, {
+                method: 'GET',
+            })
+            .then(response => response.text())
+            .then((text) => {
+                // document.getElementById('responseJsonText').value = text;
+                document.getElementById('responsePrism').innerHTML = text;
+                Prism.highlightAll();
+            });
+    } else {
+        fetch(url, {
+                method: 'POST',
+                body: data,
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+                }
+            })
+            .then(response => response.text())
+            .then((text) => {
+                // document.getElementById('responseJsonText').value = text;
+                document.getElementById('responsePrism').innerHTML = text;
+                Prism.highlightAll();
+            });
+
+    }
+
+
+});
